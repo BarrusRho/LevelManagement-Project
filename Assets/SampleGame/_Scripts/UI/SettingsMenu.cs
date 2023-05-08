49ef505a85b0f6b90d10ee99139b08180c9346d1
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-05-08 23:51:27 +0300

    - Updates settings menu for using sliders to adjust volume values

--- Assets/SampleGame/_Scripts/UI/SettingsMenu.cs
+++ Assets/SampleGame/_Scripts/UI/SettingsMenu.cs
@@ -4,6 +4,21 @@ namespace LevelManagement.UI
 {
     public class SettingsMenu : MenuBase<SettingsMenu>
     {
+        public void OnMasterVolumeChanged(float volume)
+        {
+            
+        }
+
+        public void OnSFXVolumeChanged(float volume)
+        {
+            
+        }
+
+        public void OnMusicVolumeChanged(float volume)
+        {
+            
+        }
+        
         public override void OnBackButtonPressed()
         {
             base.OnBackButtonPressed();
