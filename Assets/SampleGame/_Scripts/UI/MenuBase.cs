172483a98187805491ff333ad0d6c4f57d248bdf
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-05-22 21:07:25 +0300

    - Refactors to use direct call to singleton

--- Assets/SampleGame/_Scripts/UI/MenuBase.cs
+++ Assets/SampleGame/_Scripts/UI/MenuBase.cs
@@ -39,20 +39,11 @@ namespace LevelManagement.UI
     [RequireComponent(typeof(Canvas))]
     public abstract class MenuBase : MonoBehaviour
     {
-        protected GameManager _gameManager;
-        protected MenuManager _menuManager;
-
-        private void Start()
-        {
-            _gameManager = GameManager.Instance;
-            _menuManager = MenuManager.Instance;
-        }
-
         public virtual void OnBackButtonPressed()
         {
-            if (_menuManager != null)
+            if (MenuManager.Instance != null)
             {
-                _menuManager.CloseMenu();
+                MenuManager.Instance.CloseMenu();
             }
         }
     }
